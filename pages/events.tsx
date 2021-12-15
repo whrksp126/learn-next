a6386ab236a08832beeca4d197f7d65de8d5d459
whrksp126 <whrksp126@naver.com>
2021-12-15 22:57:07 +0900

    first commit

---
+++ pages/events.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>
+      이벤트를 만들자!
+    </div>
+  )
+}
+
+export default Events
---
+++ pages/users/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserIndexPage = () => {
+  return (
+    <div>
+      UserIndexPage
+    </div>
+  )
+}
+
+export default UserIndexPage
